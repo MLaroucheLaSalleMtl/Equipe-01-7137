227e49c16cc8f0ddad1fe1dcf0fd7b300c1129c0
Unknown <bmele57@me.com>
2019-01-30 12:12:59 -0500

    Economy 1.00
    Barely any economy, but you get a base

--- Assets/habitation.cs
+++ Assets/habitation.cs
@@ -5,4 +5,11 @@ using UnityEngine;
 public class habitation : building
 {
     public float size = 10;
+    public float Taxe = 1;
+
+    public override void PerTick()
+    {
+        base.PerTick();
+        GetOwner.GainGold((Taxe * GetOwner.totalPopulation)/GetOwner.totalPopulation);
+    }
 }
